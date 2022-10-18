9c328205ae3ca32ba3f748ff2faa7347ec23c22b
Menanya <emmiemenz@gmail.com>
2022-10-18 22:23:43 +0100

    render greeting without api

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,7 +5,26 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+import store from "./redux/configureStore";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <BrowserRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
+     
+    </BrowserRouter>
+  
+)
+
+
+
+
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root'),
+// );
---
+++ app/javascript/redux/greetingReducer.js
@@ -0,0 +1,23 @@
+const GET_GREETINGS = 'greeting/greeting/GET_GREETING'
+
+export default function greetingsReducer(state = [], action = {}){
+    switch (action.type){
+        case GET_GREETINGS:
+            return action.payload;
+            default:
+                return state;
+    }
+}
+
+function getGreeting(){
+    return async(dispatch) =>{
+        const response = await fetch('/greeting');
+        const greetings = await response.json();
+
+        dispatch({
+            type: GET_GREETINGS,
+            payload: greetings
+        })
+    }
+}
+export { getGreeting }
